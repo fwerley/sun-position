2de4e9d38da589f57b16da49a0d07bcff84e2673
Werley Ferriera <werleyfn@gmail.com>
2023-12-09 15:18:17 -0300

    Configure lib to use in npm

--- dist/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { CurrectionObject } from "./types";
-declare const p2c: (pt: Array<number>) => Array<number>;
-declare const c2p: (pt: Array<number>) => Array<number>;
-declare const degreesToRadians: (degrees: number) => number;
-declare const radiansToDegrees: (radians: number) => number;
-declare const correctionArrayHour: (array: Array<number>, values: CurrectionObject) => Array<number>;
-declare const hd2hms: (hd: number) => Array<number>;
-export { p2c, c2p, hd2hms, degreesToRadians, radiansToDegrees, correctionArrayHour };
